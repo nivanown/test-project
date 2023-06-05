257e892abb4dafaecf632b70afd48d1e71a8bab5
Nikita <32540777+nivanown@users.noreply.github.com>
2023-06-05 22:32:42 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+﻿/*- price-container -*/
+const PriceSwitcher = (function () {
+  let switcher;
+
+  const renderPrice = () => {
+    const priceContainer = document.querySelector("#price-container");
+    const monthlyPrice = document.querySelector("#monthly-price");
+    const yearlyPrice = document.querySelector("#yearly-price");
+
+    priceContainer.innerHTML = switcher.checked
+      ? yearlyPrice.innerHTML
+      : monthlyPrice.innerHTML;
+  };
+
+  const initialize = () => {
+    switcher = document.querySelector("#price-switcher");
+
+    // handle tariff change
+    switcher.addEventListener("change", renderPrice);
+
+    // initial render
+    renderPrice();
+  };
+
+  return { initialize };
+})();
+
+document.addEventListener("DOMContentLoaded", PriceSwitcher.initialize);
